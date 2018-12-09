33d1d93b62d318059822f490e778c5a07bc95b82
zhaoce <zhaoce@outlook.com>
2018-12-09 19:10:36 +0800

    edit name

---
+++ Fammer and dragon/Assets/Scripts/Prefab/Items/Consumables/Medicines/HpPotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpPotion : Consumable {
+
+    private int recoveryValue = 10;
+
+    public void Use()
+    {
+        int currentValue = this.owner.GetComponent<PlayerProperties>().HpCurrentValue;
+        int maxValue = this.owner.GetComponent<PlayerProperties>().HpMaxValue;
+
+        this.owner.GetComponent<PlayerProperties>().HpCurrentValue =
+            currentValue + recoveryValue > maxValue ? maxValue : currentValue + recoveryValue;
+
+        Destroy(this.gameObject, 1);
+    }
+}
